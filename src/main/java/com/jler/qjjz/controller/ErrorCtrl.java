e23c2e48855fdc05ba2e06b925e283f5b6e7cef8
jler <472102489@qq.com>
2017-12-15 19:48:06 +0800

    开发模式，去除错误控制器

--- src/main/java/com/jler/qjjz/controller/ErrorCtrl.java
+++ src/main/java/com/jler/qjjz/controller/ErrorCtrl.java
@@ -8,22 +8,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-@RequestMapping(value = "error")
-public class ErrorCtrl implements ErrorController {
-    private static final Logger logger = LoggerFactory.getLogger(ErrorCtrl.class);
+//@Controller
+//@RequestMapping(value = "error")
+//public class ErrorCtrl implements ErrorController {
+//    private static final Logger logger = LoggerFactory.getLogger(ErrorCtrl.class);
+//
+//    @Override
+//    public String getErrorPath() {
+//        logger.info("出错啦！进入自定义错误控制器");
+//        return "error/error";
+//    }
+//
+//    @RequestMapping
+//    public String error(Model model) {
+//        model.addAttribute("code", HttpStatus.NOT_FOUND);
+//        String errorPath = getErrorPath();
+//        return errorPath;
+//    }
 
-    @Override
-    public String getErrorPath() {
-        logger.info("出错啦！进入自定义错误控制器");
-        return "error/error";
-    }
-
-    @RequestMapping
-    public String error(Model model) {
-        model.addAttribute("code", HttpStatus.NOT_FOUND);
-        String errorPath = getErrorPath();
-        return errorPath;
-    }
-
-}
+//}
