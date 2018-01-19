8dec872211c3048f81eec4d91cf7231bd5b83158
jler <472102489@qq.com>
2018-01-19 17:04:18 +0800

    登录表单异步验证（     1.用户名验证；     2.登录按钮验证； ）

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -3,23 +3,47 @@ function loginView(){
     return false;
 }
 
+function checkId(uAcct) {
+    if(uAcct != ""){
+        if(uAcct.match("^[a-zA-Z]\\w{3,17}$")){
+            $.post("checkId",$('#login-id').serialize(),function(data){
+                if (data === "false"){
+                    $('#login-id-msg').text("该用户未注册").show();
+                    $('#login-id').css("margin","0");
+                    return "false";
+                } else {
+                    $('#login-id-msg').text("").hide();
+                    $('#login-id').css("margin","0 0 32px");
+                    return "true";
+                }
+            });
+        } else {
+            $('#login-id-msg').text("用户名不符合规范").show();
+            $('#login-id').css("margin","0");
+            return "false";
+        }
+    }
+}
+
 $('.message a').click(function () {
     $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
 });
 
 $('#login-id').blur(function () {
     var uAcct = $('#login-id').val();
-    if(uAcct != ""){
-        if(uAcct.match("^[a-zA-Z]\\w{3,17}$")){
-            $.post("checkId",$('#login-id').serialize(),function(data){
-                if (data === "false"){
-                    $('.form-control-static:first').text("该用户未注册");
-                } else {
-                    $('.form-control-static:first').text("");
-                }
-            });
-        } else {
-            $('.form-control-static:first').text("该用户未注册");
+    checkId(uAcct);
+});
+
+$('.login-form button').click(function () {
+    var uAcct = $('#login-id').val();
+    var pwd = $('#login-pwd').val();
+    if(checkId(uAcct) === 'true'){
+        if(pwd != ""){
+            $('.login-form button').submit();
+        }else {
+            $('#login-pwd-msg').text("请输入密码").show();
+            $('#login-pwd').css("margin","0");
         }
     }
+    return false;
 });
