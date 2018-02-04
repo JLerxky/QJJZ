223ef973e25f34bdf0899bcb37212dd9837eb0f3
jler <472102489@qq.com>
2018-02-04 19:09:34 +0800

    spring boot 版本更新到1.5.6

--- src/main/java/com/jler/qjjz/main.java
+++ src/main/java/com/jler/qjjz/main.java
@@ -2,7 +2,7 @@ package com.jler.qjjz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
